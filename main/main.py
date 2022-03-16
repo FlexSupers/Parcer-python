3a27955c26130b575cc088cdaa835beb6974d8ca
FlexSupers <flexsupers@gmail.com>
2022-03-16 18:30:33 +0700

    Добавил рабочее окно для программы

--- main/main.py
+++ main/main.py
@@ -1,3 +1,8 @@
+from cProfile import label
+from kivy.app import App
+from kivy.uix.label import Label
+
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,10 +12,36 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
 names = soup.find('table', class_='h7vnx2-2 czTsgW cmc-table').find_all('p', class_='sc-1eb5slv-0 iworPT')
 prices = soup.find('table', class_='h7vnx2-2 czTsgW cmc-table').find_all('div' ,class_='sc-131di3y-0 cLgOOr')
-#print(prices)
-#print(names)
+market_caps = soup.find('table', class_='h7vnx2-2 czTsgW cmc-table').find_all('span', class_='sc-1ow4cwt-1 ieFnWP')
+
+n=[]
+p=[]
+m_c=[]
 for name in names:
-    print(name.text)
-    
+    n.append(name.text)
+    #print(name.text)
+
 for price in prices:
-    print(price.text)
+    p.append(price.text)
+    #print(price.text)
+
+for market_cap in market_caps:
+    m_c.append(market_cap.text)
+    #print(market_cap.text)
+
+
+print(n,'\n',p,'\n',m_c)
+
+class MainApp(App):
+    def build(self):
+        label = Label(text = 'Привет! Я программа с актуальными криптовалютами. Что ты хочешь найти?',
+        size_hint = (1, 1),
+        pos_hint = {'center_x': .5, 'center_y': .9}
+        )
+
+        return label
+
+
+
+if __name__ == '__main__':
+    MainApp().run()
