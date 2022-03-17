899c12100b8c4fe5e5ff2055fcf875ae286781ac
FlexSupers <flexsupers@gmail.com>
2022-03-17 21:22:35 +0700

    Начал делать класс

--- main/main.py
+++ main/main.py
@@ -1,59 +1,50 @@
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
-
-
 import requests
 from bs4 import BeautifulSoup
 
 url = "https://coinmarketcap.com/"
-
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
 names = soup.find('table', class_='h7vnx2-2 czTsgW cmc-table').find_all('p', class_='sc-1eb5slv-0 iworPT')
 prices = soup.find('table', class_='h7vnx2-2 czTsgW cmc-table').find_all('div' ,class_='sc-131di3y-0 cLgOOr')
 market_caps = soup.find('table', class_='h7vnx2-2 czTsgW cmc-table').find_all('span', class_='sc-1ow4cwt-1 ieFnWP')
 
-n=[]
-p=[]
-m_c=[]
+class Kripta:
+    n=[]
+    p=[]
+    m_c=[]
+    menu=""
+
+    
+
+
 for name in names:
     n.append(name.text)
-    #print(name.text)
-
+    
 for price in prices:
     p.append(price.text)
-    #print(price.text)
-
+    
 for market_cap in market_caps:
     m_c.append(market_cap.text)
-    #print(market_cap.text)
 
 
-print(n,'\n',p,'\n',m_c)
+while True:
+    print('\n' 
+    '1. Увидеть полный список криптовалют''\n'
+    '2. Найти информацию об определенной криптовалюте' '\n' 
+    '3. Закончить программу')
 
-class Container(GridLayout):
-    pass
-
-class MainApp(App):
-    def build(self):
-        #label = Label(text = 'Привет! Я программа с актуальными криптовалютами. Что ты хочешь найти?',
-        #size_hint = (None, None),
-        #pos_hint = {'center_x': .5, 'center_y': .9}
-        #)
-    
-        bl = GridLayout(cols = 2, padding = [30], spacing = 200)
-        button1 = Button(text = 'Поиск Криптовалюты', size_hint = (None, None))
-        button2 = Button(text = 'Актуальные криптовалюты', size_hint = (None, None))
-        bl.add_widget(button1)
-        bl.add_widget(button2)
-
-        #return label
-        return bl
+    menu=input()
+    if menu == "1":
+        print(n,'\n',p,'\n',m_c)
+        
+    elif menu == "2":
+        print("Введите название криптовалюты с условием регистра (например Bitcoin)")
+        x=input()
+        index=n.index(x)
+        print(n[index],'Price:',p[index], 'Market_cap:',m_c[index])
+    elif menu == "3":
+        break
         
 
 
-
-if __name__ == '__main__':
-    MainApp().run()
+    
