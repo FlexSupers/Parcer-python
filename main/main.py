2144138630a7ec56544b1e89cda0e394e8b084f8
FlexSupers <flexsupers@gmail.com>
2022-04-12 17:42:42 +0700

    Add window with 2 buttons

--- main/main.py
+++ main/main.py
@@ -1,5 +1,20 @@
+from cProfile import label
 import requests
 from bs4 import BeautifulSoup
+import dearpygui.dearpygui as dpg
+
+dpg.create_context()
+dpg.create_viewport(title ='Custom Title', width= 600, height= 300)
+
+with dpg.window(label= 'Python Parcer'):
+    dpg.add_text("Hi, this is KryptoParcer")
+    dpg.add_button(label="Search")
+    dpg.add_button(label= 'Exit')
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
 
 url = "https://coinmarketcap.com/"
 response = requests.get(url)
@@ -8,13 +23,13 @@ names = soup.find('table', class_='h7vnx2-2 czTsgW cmc-table').find_all('p', cla
 prices = soup.find('table', class_='h7vnx2-2 czTsgW cmc-table').find_all('div' ,class_='sc-131di3y-0 cLgOOr')
 market_caps = soup.find('table', class_='h7vnx2-2 czTsgW cmc-table').find_all('span', class_='sc-1ow4cwt-1 ieFnWP')
 
-class Kripta:
-    n=[]
-    p=[]
-    m_c=[]
-    menu=""
 
-    
+n=[]
+p=[]
+m_c=[]
+menu=""
+
+
 
 
 for name in names:
