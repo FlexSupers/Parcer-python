0b2b43f7fc64666ef7ed31cbc5855903cbc198ea
FlexSupers <flexsupers@gmail.com>
2022-04-19 21:26:33 +0700

    Add window for parcer
    Сделано рабочее окно для парсера

--- main/main.py
+++ main/main.py
@@ -2,6 +2,9 @@ from cProfile import label
 import requests
 from bs4 import BeautifulSoup
 import dearpygui.dearpygui as dpg
+import sys
+
+
 
 url = "https://coinmarketcap.com/"
 response = requests.get(url)
@@ -11,10 +14,12 @@ prices = soup.find('table', class_='h7vnx2-2 czTsgW cmc-table').find_all('div' ,
 market_caps = soup.find('table', class_='h7vnx2-2 czTsgW cmc-table').find_all('span', class_='sc-1ow4cwt-1 ieFnWP')
 
 
-n=[]
-p=[]
-m_c=[]
+n = []
+p = []
+m_c = []
 menu=""
+
+
 
 
 
@@ -37,39 +42,64 @@ with open(r"data.txt", "w") as file:
 file.close()
 
 
+def allkrypta():
+    print("Name:",n)
+    print("Price:",p)
+    print("Cap:",m_c)
+
+def search():
+    print("Введите название криптовалюты с условием регистра (например Bitcoin)")
+    x=input()
+    index=n.index(x)
+    print(n[index],'Price:',p[index], 'Market_cap:',m_c[index])
+
+    
+   
+
+def ex():
+    sys.exit()
+
+
+
 
 dpg.create_context()
-vp = dpg.create_viewport(title='Parcer', width=600, height=200)
-
-with dpg.window(label = 'Python Parcer'):
+vp = dpg.create_viewport(title='Parcer', width=1400, height=600)
+with dpg.window(label = 'Python Parcer', id = "main window"):
     dpg.add_text("Hi, this is KryptoParcer")
-    dpg.add_button(label = 'All Krypto')
-    dpg.add_button(label ="Search")
-    dpg.add_button(label = 'Exit')
+    dpg.add_button(label = 'All Krypto', callback = allkrypta)
+    dpg.add_button(label ="Search", callback = search)
+    dpg.add_button(label = 'Exit', callback = ex)
+    #dpg.add_combo(n) # лист с выбором крипты
+    #dpg.add_plot() # можно сделать график падения и поднять крипты)
+    #dpg.add_listbox(n)
+    dpg.add_text(str(n))
+    dpg.add_text(str(m_c))
+    dpg.add_text(str(p))
 
+dpg.set_primary_window("main window", True)
 dpg.setup_dearpygui(viewport=vp)
 dpg.show_viewport()
 dpg.start_dearpygui()
 
 
-while True:
-    print('\n' 
-    '1. Увидеть полный список криптовалют''\n'
-    '2. Найти информацию об определенной криптовалюте' '\n' 
-    '3. Закончить программу')
+# while True:
+#     print('\n' 
+#     '1. Увидеть полный список криптовалют''\n'
+#     '2. Найти информацию об определенной криптовалюте' '\n' 
+#     '3. Закончить программу')
 
-    menu=input()
-    if menu == "1":
-        print(n,'\n',p,'\n',m_c)
+#     menu=input()
+#     if menu == "1":
+#         print(n,'\n',p,'\n',m_c)
         
-    elif menu == "2":
-        print("Введите название криптовалюты с условием регистра (например Bitcoin)")
-        x=input()
-        index=n.index(x)
-        print(n[index],'Price:',p[index], 'Market_cap:',m_c[index])
-    elif menu == "3":
-        break
-        
+#     elif menu == "2":
+#         print("Введите название криптовалюты с условием регистра (например Bitcoin)")
+#         x=input()
+#         index=n.index(x)
+#         print(n[index],'Price:',p[index], 'Market_cap:',m_c[index])
+#     elif menu == "3":
+#         break
+       
 
 
     
